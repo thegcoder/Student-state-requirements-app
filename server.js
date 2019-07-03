11529fa5105c560eab4ce6346fe2eb0186208ff1
Tai Glover <taigloverslaton@gmail.com>
2019-07-03 11:13:17 -0400

    renamed views files

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * This is the template for a server.js file.  Follow the steps below and read
  * the comments for creating your own (or you can just copy this file).
  */
@@ -13,12 +13,12 @@ const app = express()
 const methodOverride = require('method-override')
 
 /* Step 2
- * 
+ *
  * import routers from controllers/
  *
  */
-const { shopRouter } = require('./controllers/shop.js')
-const { foodRouter } = require('./controllers/food.js')
+const { stateRouter } = require('./controllers/state.js')
+const { studentRouter } = require('./controllers/student.js')
 
 
 /* Step 3
@@ -27,33 +27,33 @@ const { foodRouter } = require('./controllers/food.js')
  */
 
 /* Step 3.a
- * ...to parse the body of the HTTP requests from a URL encoded string 
+ * ...to parse the body of the HTTP requests from a URL encoded string
  */
 app.use(express.urlencoded())
 
-/* Step 3.b 
+/* Step 3.b
  *
- * ...to parse the body of the HTTP requests from a JSON string  
- * replace the above line with this 
+ * ...to parse the body of the HTTP requests from a JSON string
+ * replace the above line with this
  */
 app.use(express.json())
 
-/* Step 3.b 
+/* Step 3.b
  *
- * add method-override middleware for "faking" DELETE and PUT/PATCH requests 
+ * add method-override middleware for "faking" DELETE and PUT/PATCH requests
  */
 app.use(methodOverride('_method'))
 
 /* Step 3.c
  *
  * use the `./public` directory to host static resources such as css and
- * image files 
+ * image files
  */
 app.use(express.static(__dirname+"/public"))
 
 /* Step 3.b
  *
- * set the view engine of express to use the hbs (handlebars) package 
+ * set the view engine of express to use the hbs (handlebars) package
  */
 app.set('view engine', 'hbs')
 
@@ -63,16 +63,16 @@ app.set('view engine', 'hbs')
  * add router for the application to use. The first argument is a prefix to all
  * the paths defined in the router.
  */
-app.use('/shops', shopRouter)
-app.use('/shops/:shopId/food', foodRouter)
+app.use('/state', stateRouter)
+app.use('/state/:stateId/student', studentRouter)
 
 /* Step 5
  *
  * Set the port the server is to run on
  *
- * NOTE: keep these lines at the bottom of the file 
+ * NOTE: keep these lines at the bottom of the file
  */
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT || 3000
 
 /* Step 6
  *
